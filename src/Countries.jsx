cb428377cfca0ce029bc958db49432d4ffb90dfa
Michael Brook <michael@pitchly.com>
2025-10-13 13:48:31 -0500

    Displaying list of countries now from direct API call, and displaying it pretty with some styling. Need to still show country details.

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,6 +1,15 @@
 import { useState, useEffect } from 'react'
 import './Countries.css'
 
+/**
+ * Formats a number with commas as thousands separators
+ * @param {number} num - The number to format
+ * @returns {string} The formatted number string
+ */
+function formatNumberWithCommas(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 function Countries() {
     const [countries, setCountries] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -12,7 +21,7 @@ function Countries() {
             try {
                 setLoading(true);
                 const response = await fetch(
-                    'https://restcountries.com/v3.1/all?fields=name,capital,population'
+                    'https://restcountries.com/v3.1/all?fields=name,flag,population,region,capital'
                 );
                 
                 if (!response.ok) {
@@ -20,11 +29,21 @@ function Countries() {
                 }
                 
                 const data = await response.json();
+
+                console.log(data);
                 
                 // Transform the API response to match our component's expected format
                 const formattedCountries = data.map(country => ({
-                    name: country.name.common,
-                    capital: country.capital ? country.capital[0] : 'N/A',
+                    // Object where name.common holds the common name of the country
+                    name: country.name,
+                    // The flag of the country as a string (special character)
+                    flag: country.flag,
+                    // The continent or region the country belongs to
+                    region: country.region,
+                    // Array of capital cities as strings. Some countries may have multiple
+                    // capital cities. Some may not have any. So take that into account.
+                    capitals: country.capital,
+                    // The population of the country as a number
                     population: country.population
                 }));
                 
@@ -52,9 +71,23 @@ function Countries() {
             <div className="countries-list">
                 {countries.map((country, index) => (
                     <div key={index} className="country">
-                        <h2>{country.name}</h2>
-                        <p>{country.capital}</p>
-                        <p>{country.population}</p>
+                        <div className="country-flag">
+                            {country.flag}
+                        </div>
+                        <div className="country-details">
+                            <div className="country-name">{country.name.common}</div>
+                            <div className="country-region">Region: {country.region}</div>
+                            <div className="country-capitals">
+                                {country.capitals.length===0 ?
+                                    'Capital: N/A'
+                                : country.capitals.length > 1 ?
+                                    `Capitals: ${country.capitals.join(', ')}`
+                                :
+                                    `Capital: ${country.capitals[0]}`
+                                }
+                            </div>
+                            <div className="country-population">Population: {formatNumberWithCommas(country.population)}</div>
+                        </div>
                     </div>
                 ))}
             </div>
